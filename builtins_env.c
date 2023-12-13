0a9072279d25d95c859000ec4f86db4e902140c4
muhoro-wandia <61504824+Muhos-wandia@users.noreply.github.com>
2023-12-13 20:46:53 +0300

    Update builtins_env.c

--- builtins_env.c
+++ builtins_env.c
@@ -2,7 +2,7 @@
 
 /**
  * builtin_env - shows the environment where the shell runs
- * @data: struct for the program's data
+ * @data: struct for the program's data.
  * Return: zero if sucess, or other number if its declared in the arguments
  */
 int builtin_env(data_of_program *data)
